4b4d5c06921c31d17d9fa9a1e7ec9a3b49d3033c
Lotzi Bölöni <lotzi.boloni@gmail.com>
2024-12-19 16:10:30 -0500

    Created framework for experiment-specific configurations

--- src/settings.py
+++ src/settings.py
@@ -36,7 +36,7 @@ class Config:
             with main_config.open("rt") as handle:
                 main_config = yaml.safe_load(handle)
             configpath = main_config["configpath"]
-            print(f"Loading machine-specific config file: {configpath}")
+            print(f"Loading machine-specific config file: {configpath}", flush=True)
             configpath = Path(configpath)
             if not configpath.exists():
                 raise Exception(f"Missing machine-specific config file: {configpath}")
@@ -46,3 +46,23 @@ class Config:
 
     def __getitem__(self, key):
         return self.values[key]
+    
+    def get_experiment(self, name):
+        """Returns an experiment configuration, which is the 
+        mixture between the system-dependent configuration and the system independent configuration."""
+        # Load the system independent configuration
+        current_directory = Path(__file__).resolve().parent
+        experiment_sys_indep = Path(current_directory, "experiment_configs", name + ".yaml")
+        if not experiment_sys_indep.exists():
+            raise Exception(f"Missing experiment system independent config file {experiment_sys_indep}")
+        with experiment_sys_indep.open("rt") as handle:
+            indep_config = yaml.safe_load(handle)
+        # Load the system dependent configuration
+        experiment_directory = Path(self.values["experiment_system_dependent_dir"])
+        experiment_sys_dep = Path(experiment_directory, name + "_sysdep.yaml")
+        if not experiment_sys_dep.exists():
+            raise Exception(f"Missing experiment system dependent config file {experiment_sys_dep}")
+        with experiment_sys_dep.open("rt") as handle:
+            dep_config = yaml.safe_load(handle)
+        exp_config = indep_config | dep_config
+        return exp_config
