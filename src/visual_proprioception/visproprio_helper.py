2195293c3f17999cb671aeb083824cc836d3604d
Lotzi Bölöni <lotzi.boloni@gmail.com>
2025-01-08 19:14:40 -0500

    Conversion of existing code of conv-vae and visual proprioception towards fully experiment/run driven

---
+++ src/visual_proprioception/visproprio_helper.py
@@ -0,0 +1,78 @@
+"""
+visproprio_helper.py
+
+Helper functions for visual proprioception training
+"""
+
+import pathlib
+import torch
+from settings import Config
+from behavior_cloning.demo_to_trainingdata import BCDemonstration
+
+
+def load_demonstrations_as_proprioception_training(sp, task, proprioception_input_file, proprioception_target_file):
+    """
+    FIXME: it is not clear if this is the one where this data is put together - if that is the case, this is actually not the right function to call
+
+    Iterates over all the demonstrations of a task. Loads the images and the corresponding positions. Preprocesses the images with the sensor processor sp passed. Saves these data into proprioception_input_file, proprioception_target_file.
+
+    These data are cached, if the files exists, it does not iterate over the demonstrations, just loads the cached data. Remove those files to recalculate.
+
+    Shuffles the dataset thus created, and splits it into training and validation data. 
+
+    Returns a dictionary with the training and validation data. 
+    """
+    retval = {}
+    if proprioception_input_file.exists():
+        retval["inputs"] = torch.load(proprioception_input_file, weights_only=True)
+        retval["targets"] = torch.load(proprioception_target_file, weights_only=True)
+    else:
+        demos_dir = pathlib.Path(Config()["demos"]["directory"])
+        task_dir = pathlib.Path(demos_dir, "demos", task)
+        
+        inputlist = []
+        targetlist = []
+
+        for demo_dir in task_dir.iterdir():
+            if not demo_dir.is_dir():
+                pass
+            bcd = BCDemonstration(demo_dir, sensorprocessor=sp)
+            print(bcd)
+            z, a = bcd.read_z_a()
+            # normalize the actions
+            print(z.shape)
+            print(a.shape)
+            anorm = np.zeros(a.shape, np.float32)
+            for i in range(a.shape[0]):
+                rp = RobotPosition.from_vector(a[i])
+                anorm[i,:] = rp.to_normalized_vector()        
+            # FIXME the repeated name for inputs and targets
+            print(z.shape)
+            print(anorm.shape)
+
+            for i in range(z.shape[0]):
+                inp = torch.from_numpy(z[i])
+                tgt = torch.from_numpy(anorm[i])
+                inputlist.append(inp)
+                targetlist.append(tgt)
+
+        retval["inputs"] = torch.stack(inputlist)
+        retval["targets"] = torch.stack(targetlist)
+        torch.save(retval["inputs"], proprioception_input_file)
+        torch.save(retval["targets"], proprioception_target_file)
+
+    # Separate the training and validation data. 
+    # We will be shuffling the demonstrations 
+    length = retval["inputs"].size(0)
+    rows = torch.randperm(length) 
+    shuffled_inputs = retval["inputs"][rows]
+    shuffled_targets = retval["targets"][rows]
+
+    training_size = int( length * 0.67 )
+    retval["inputs_training"] = shuffled_inputs[1:training_size]
+    retval["targets_training"] = shuffled_targets[1:training_size]
+
+    retval["inputs_validation"] = shuffled_inputs[training_size:]
+    retval["targets_validation"] = shuffled_targets[training_size:]
+
+    return retval
---
+++ src/visual_proprioception/visproprio_models.py
@@ -0,0 +1,36 @@
+"""
+visproprio_models.py
+
+Regression models for visual proprioception. These models take as input 
+a latent representation of the visual input, and create as the output of regression the positional information of the robot. 
+
+They are parameterized by an experiment yaml, which defines the internal structure and the location where the model will be saved and restored from. 
+
+For the time being, we assume that the positional information is a normalized form of RobotPosition.
+"""
+import torch.nn as nn
+
+
+
+class VisProprio_SimpleMLPRegression(nn.Module):
+    """A simple two-hidden layer MLP-based model for 
+    visual proprioception regression """
+
+    def __init__(self, exp):
+        super(VisProprio_SimpleMLPRegression, self).__init__()
+
+        self.input_size = exp["encoding_size"]
+        self.hidden_size_1 = exp["regressor_hidden_size_1"]
+        self.hidden_size_2 = exp["regressor_hidden_size_2"]
+        self.output_size = exp["output_size"] # normally, 6
+
+        self.model = nn.Sequential(
+            nn.Linear(self.input_size, self.hidden_size1),
+            nn.ReLU(),
+            nn.Linear(self.hidden_size1, self.hidden_size2),
+            nn.ReLU(),
+            nn.Linear(self.hidden_size2, self.output_size)
+        )
+
+    def forward(self, x):
+        return self.model(x)
